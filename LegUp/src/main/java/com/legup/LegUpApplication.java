3df5349adf2b6efed909ffd93be72e58157b97c9
Chandrika1211 <78041793+Chandrika1211@users.noreply.github.com>
2022-09-19 21:52:32 +0530

    Add files via upload

---
+++ LegUp/src/main/java/com/legup/LegUpApplication.java
@@ -0,0 +1,13 @@
+package com.legup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegUpApplication.class, args);
+	}
+
+}
---
+++ LegUp/src/test/java/com/legup/LegUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
