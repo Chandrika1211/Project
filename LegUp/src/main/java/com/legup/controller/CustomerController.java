c882382ad1d6056388b5defbf0273d437458b944
Rohan's PC <rohanshinde0017@gmail.com>
2022-09-21 20:52:10 +0530

    Rohan
    Signed-off-by: Rohan's PC <rohanshinde0017@gmail.com>

--- LegUp/src/main/java/com/legup/controller/CustomerController.java
+++ LegUp/src/main/java/com/legup/controller/CustomerController.java
@@ -1,8 +1,15 @@
 package com.legup.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
+	@GetMapping("/")
+	public String welcome() {
+		return "Hello Rohan!!  Current time is  "+ LocalDateTime.now();
+	}
 }
